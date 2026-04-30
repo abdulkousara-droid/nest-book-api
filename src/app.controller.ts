dfde97d0d48f31b7096e350e021332075470635e
abdulkousara-droid <abdulkader.kousara@gmail.com>
2026-04-30 20:11:48 +0200

    adding post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put} from '@nestjs/common';
 import { BooksService } from './app.service';
 import { Book } from './FakeDatabase'
 
@@ -16,4 +16,19 @@ export class BooksController {
     const bookID = +id
     return this.booksService.findById(bookID)
   }
+
+  @Post()
+  addBook(@Body() book: Partial<Book>): Book | undefined{
+    const bookData = book
+
+    if (!book.author || !book.title || !book.publicationYear) return undefined
+
+    return this.booksService.create(bookData)
+  }
+
+  @Put(':id')
+  updateBook(@Param('id')id: string, @Body() book: Partial<Book>): Book | undefined{
+    return this.booksService.update(+id, book)
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -10,4 +10,33 @@ export class BooksService {
   findById(bookId: number): Book | undefined{
     return books.find((book) => book.id === bookId);
   }
+
+  create(book: Partial<Book>){
+    const newID = books[books.length - 1].id + 1
+
+    const newBook: Book = {
+      id: newID,
+      author: book.author ?? '',
+      title: book.title ?? '',
+      publicationYear: book.publicationYear ?? 0
+    }
+
+    books.push(newBook);
+
+    return newBook;
+  }
+
+  update(bookID: number, updateBookFields: Partial<Book>): Book | undefined {
+    const currentBook = books.find((book) => book.id === bookID);
+    const updatedBook = { ...updateBookFields, ...currentBook };
+    books.map((book)=> {
+      if (book.id === bookID) {
+        return updatedBook;
+      }else {
+        return book;
+      }
+    });
+
+    return updatedBook;
+  }
 }
